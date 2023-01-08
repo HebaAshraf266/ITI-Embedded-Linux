13283479f2d3498a77aefa5f9c7a1c13bf39a2a9
Heba Ashraf <115734048+HebaAshraf266@users.noreply.github.com>
2023-01-08 05:15:03 +0200

    Add files via upload

---
+++ PythonProjects/Calculator.py
@@ -0,0 +1,65 @@
+# Calculator -- programmer,  standard (Using eval())
+
+print("\nWelcome to Caluclator!")
+Status=True
+while Status:
+   print("Standard mode                      Press 1")
+   print("Programmer mode                    Press 2")
+   print("Exit                               Press 0")
+
+   print("-"*50) #--> Separator
+   user_mode=int(input("Enter your choice for mode: "))
+   print("-"*50) #--> Separator
+
+   if user_mode==1:
+        print("Enter first number ")
+        print("Enter operation + - * / % ")
+        print("Enter other numbers with other operation ")
+        Result=eval(input("Enter a math exPression: "))
+        print("Result of operation= ",Result)
+        print("-"*50) #--> Separator
+
+
+   elif user_mode==2:
+        print("For Logical operations             Press 1")
+        print("For Binary conversion              Press 2")
+        print("For Hexdecimal conversion          Press 3")
+        print("Exit                               Press 0")
+
+        print("-"*50) #--> Separator
+        prog_mode=int(input("Enter your choice: "))
+        print("-"*50) #--> Separator
+
+        if prog_mode==1:
+            print("For Logic NOT                      Press 1")
+            print("For Other logical operations       Press 2")
+            Choice=int(input("Enter your chioce: "))
+            print("-"*50) #--> Separator
+            if Choice==1:
+                result=eval(input("Enter the number: "))
+                print("Result of logical NOT= ",(~result))
+                print("Result in binary= ",bin(~ result))
+                print("-"*50) #--> Separator
+            elif Choice==2:
+                print("Enter first number") 
+                print("Enter logic operator , option for operators: & | ^ >> << ")
+                print("Enter other numbers operation with other operator")
+                Result=eval(input("Enter a math exPression: "))
+                print("Result of operation= ",Result)   
+                print("Result in binary= ",bin(Result))
+                print("-"*50) #--> Separator 
+
+        elif prog_mode==2:
+            num=int(input("Enter your decimal number to convert to binary: "))
+            print(f"Binary number of Decimal number {num} = {bin(num)}")
+            print("-"*50) #--> Separator
+
+        elif prog_mode==3: 
+            Num=int(input("Enter your decimal number to convert to hexdecimal: ")) 
+            print(f"hexdecimal value of Decimal number {Num} = {hex(Num)}")
+            print("-"*50) #--> Separator
+        
+        else:
+            Status=False              
+   else: 
+       break
---
+++ PythonProjects/GroceryShop.py
@@ -0,0 +1,122 @@
+#Grocerry Shop 
+
+import time
+
+Grocerry_Items={                       # -->Dictionary 
+"Types":["Apple","Banana","Cherry"],
+"Quantity":[40, 30, 70],
+"Price":[15, 20, 25]
+}
+
+Customer_Dict={
+"Bag": [],
+"Quantity": [],
+"Bill":[]
+}
+
+print("\nWelcome to ITI shop!")
+Status=True
+while Status:
+ print("For Owener mode           press 1")
+ print("For Customer mode         press 2")
+ print("For Exist                 press 0")
+
+ print("-"*40) #--> Separator
+ mode_choice=int(input("Enter your mode choice "))
+ print("-"*40) #--> Separator
+
+
+ if mode_choice==2:                                  #--> Customer mode 
+        print("To show our products      press 1")
+        print("To Exist                  press 0")
+
+        print("-"*40) #--> Separator
+        customer_choice=int(input("Enter your choice: "))
+        print("-"*40) #--> Separator
+
+        if customer_choice==1:
+            print("Shop products ",Grocerry_Items["Types"])
+            print("Quantity of each type ",Grocerry_Items["Quantity"])
+            print("Price for each type ",Grocerry_Items["Price"])
+            loop=True
+            while loop:
+             print("-"*40) #--> Separator
+             print("To Buy from our product     press 1")
+             print("To print the bell           press 2") 
+             print("Finished                    press 0")
+             print("-"*40) #--> Separator
+             x=int(input("Enter your choice: "))
+             print("-"*40) #--> Separator
+             if x==1:
+                Fruit_type=input("Enter Fruit type you want to buy: ")
+                Customer_Dict["Bag"].append(Grocerry_Items["Types"][Grocerry_Items["Types"].index(Fruit_type)])  
+                print(Customer_Dict["Bag"])
+
+                quantity=int(input("Enter the quantity you want to buy: "))
+                Customer_Dict["Quantity"].append(quantity)
+                print("Quantity= " ,Customer_Dict["Quantity"])
+
+                Grocerry_Items["Quantity"][Grocerry_Items["Types"].index(Fruit_type)] -=quantity
+                print("Available quantity for each type= ",Grocerry_Items["Quantity"])
+
+             elif  x==2:
+                bill = 0 
+                for i in range(len(Customer_Dict["Bag"])):
+                    #To calculate total cost
+                    bill += Grocerry_Items["Price"][Grocerry_Items["Types"].index(Customer_Dict["Bag"][i])] * Customer_Dict["Quantity"][i]
+                print("Total cost= ",end="")
+                print(bill)
+
+             elif x==0:
+                 loop=False
+                
+                
+ elif mode_choice==1:   
+            Loop=True
+            while Loop:                                #--> Owner mode 
+             print("Show products             press 1")
+             print("Add new products          press 2")
+             print("Change cost               press 3")
+             print("Exist                     press 0")
+            
+             print("-"*40) #--> Separator
+             owner_choice=int(input("Enter your choice: "))
+             print("-"*40) #--> Separator
+
+             if owner_choice==1:
+                print("Shop products ",Grocerry_Items["Types"])
+                print("-"*40) #--> Separator
+
+             elif owner_choice==2:
+                Add_choice=input("Enter name of the new product: ")
+                Grocerry_Items["Types"].append(Add_choice)
+                
+                Add_choiceQ=int(input("Enter quantity of the new product: "))
+                Grocerry_Items["Quantity"].append(Add_choiceQ)
+
+                Add_choiceP=int(input("Enter cost of the new product: "))
+                Grocerry_Items["Price"].append(Add_choiceP)
+
+                print(Grocerry_Items)
+                print("-"*40) #--> Separator
+
+             elif owner_choice==3:
+                print("Products: ",Grocerry_Items["Types"])
+                print("Cost of Products: ",Grocerry_Items["Price"])
+
+                print("-"*40) #--> Separator
+                i =input("Enter the element you want to change it's price: ")
+                New_cost=int(input("Enter the new price: "))
+                Grocerry_Items["Price"][Grocerry_Items["Types"].index(i)]=New_cost #--> To link price with its product 
+
+                #print("Products: ",Grocerry_Items["Types"])
+                print("Cost of Products: ",Grocerry_Items["Price"])
+				print("-"*40) #--> Separator
+
+
+             elif owner_choice==0:
+                Loop=False  
+            
+ else: 
+    Status=False
+
